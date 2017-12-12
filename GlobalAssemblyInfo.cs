530fce1c94b118836a3e7045ad1681e9accd89ad
Leonard Thieu <leonard.thieu@gmail.com>
2017-12-12 16:33:29 -0500

    chore: Experiment with versionless master (and release branches).

--- GlobalAssemblyInfo.cs
+++ GlobalAssemblyInfo.cs
@@ -1,7 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyVersion("9.0.1.0")]
+[assembly: AssemblyVersion("0.0.0.0")]
 
 [assembly: AssemblyCopyright("Copyright © Leonard Thieu 2017")]
 [assembly: AssemblyProduct("toofz")]
